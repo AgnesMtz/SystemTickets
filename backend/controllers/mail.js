3aa77db2ac63de2d69df82328a921d30c7031809
carrillo2202 <carrillloolmedobrayan@gmail.com>
2023-10-05 07:50:33 -0600

    Este es el mensaje correcto

---
+++ backend/controllers/mail.js
@@ -0,0 +1,17 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, 
+    auth: {
+      user: process.env.E_MAIL, 
+      pass: process.env.EPASSWORD, 
+    },
+  });
+
+  transporter.verify().then(()=>{
+    console.log("Listo para mandar correos")
+}).catch(error => {
+  console.log("Error al conectar con servidor para mandar correos")
+})
